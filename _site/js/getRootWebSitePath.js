4d1b70474f79ec03e69f0e1ce63e17a9e5687d9b
amsantac <amsantac@gmail.com>
2015-08-08 18:59:23 -0500

    add getRootWebSitePath.js

---
+++ _site/js/getRootWebSitePath.js
@@ -0,0 +1,11 @@
+function getRootWebSitePath()
+{
+var location = document.location.toString();
+var applicationNameIndex = location.indexOf('/', location.indexOf('://') + 3);
+var applicationName = location.substring(0, applicationNameIndex) + '/';
+var webFolderIndex = location.indexOf('/', location.indexOf(applicationName) + applicationName.length);
+var webFolderFullPath = location.substring(0, webFolderIndex);
+return webFolderFullPath;
+}
+var rootDir = getRootWebSitePath();
+print rootDir;
