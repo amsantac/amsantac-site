40d8688b44d196bc509a43e4d9768f7eeab4dd9f
amsantac <amsantac@gmail.com>
2015-08-08 19:09:28 -0500

    try path with location

--- js/getRootWebSitePath.js
+++ js/getRootWebSitePath.js
@@ -5,6 +5,7 @@ var applicationNameIndex = location.indexOf('/', location.indexOf('://') + 3);
 var applicationName = location.substring(0, applicationNameIndex) + '/';
 var webFolderIndex = location.indexOf('/', location.indexOf(applicationName) + applicationName.length);
 var webFolderFullPath = location.substring(0, webFolderIndex);
-return webFolderFullPath;
+//return webFolderFullPath;
+return location;
 }
 var rootDir = getRootWebSitePath();
